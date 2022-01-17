bef3ded2d2fba16567422e90933dd855dfdef1ca
Bernard Dubuisson <bernard@dub.be>
2022-01-18 00:10:56 +0100

    styling+varia
    some nice styling on mobile screen
    flash notice margins and remove duplicates
    bugfix profanity redirect from result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(profanity:params[:profanity])
     flash[:notice] = "Préférences mises à jour vers : #{@user.profanity}"
-    redirect_back fallback_location: root_path
+    unless request.headers["Referer"].include? "result"
+      redirect_back fallback_location: root_path
+    else
+      redirect_to root_path
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
   
   def links_to_test_and_reverse(lesson)
-		"<div class='btn-group col width-auto' role='group' aria-label='Link to play'>\n
+		"<div class='btn-group col width-auto mb-2 mb-sm-0' role='group' aria-label='Link to play'>\n
 		  #{link_to "<-", play_lesson_path(id:lesson,reverse:'reverse'), class:"btn btn-lg btn-warning"}\n
 		  <button type='button' class='btn btn-lg btn-warning' inactive>Commencer le test :</button>\n
 		  #{link_to "->", play_lesson_path(id:lesson), class:"btn btn-lg btn-warning"}\n
