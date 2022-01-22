1191c2b0cedca82cc7bf600167dd64e67e6c4e65
Bernard Dubuisson <bernard@dub.be>
2022-01-22 20:54:29 +0100

    add https redirection for all requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_to_https
+  
+  def redirect_to_https
+      redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
+  end
+  
 end
